4d00c7f6cc622364bf7e658ee6674adc12b344a4
q-13t <vova29092002@gmail.com>
2023-06-28 18:50:38 +0300

    IMP: 1514. Path with Maximum Probability

--- src/main/java/le/test/LeetCode/Solution.java
+++ src/main/java/le/test/LeetCode/Solution.java
@@ -4383,4 +4383,28 @@ public class Solution extends GuessGame {
         // command = command.replaceAll("[(][)]", "o");
         return command.replaceAll("[(][)]", "o").replaceAll("[(]al[)]", "al");
     }
+
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        double dp[] = new double[n];
+        dp[start] = 1.0;
+        for (int i = 0; i < n - 1; i++) {
+            boolean done = false;
+            for (int j = 0; j < edges.length; j++) {
+                int e1 = edges[j][0];
+                int e2 = edges[j][1];
+                double p = succProb[j];
+                if (dp[e2] * p > dp[e1]) {
+                    dp[e1] = dp[e2] * p;
+                    done = true;
+                }
+                if (dp[e1] * p > dp[e2]) {
+                    dp[e2] = dp[e1] * p;
+                    done = true;
+                }
+            }
+            if (!done)
+                break;
+        }
+        return dp[end];
+    }
 }
