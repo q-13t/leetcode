240e0302c12d51133c423e82e9639d2dd917bcfb
q-13t <vova29092002@gmail.com>
2025-02-08 12:14:55 +0200

    IMP: 2349. Design a Number Container System

---
+++ src/main/c++/LeetCode/NumberContainers.h
@@ -0,0 +1,36 @@
+#ifndef NUMBER_CONTAINERS
+#define NUMBER_CONTAINERS
+
+#include "map"
+#include "queue"
+#include "vector"
+using namespace std;
+
+class NumberContainers {
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> indexes;
+    unordered_map<int, int> values;
+
+   public:
+    NumberContainers() {
+    }
+
+    void change(int index, int number) {
+        values[index] = number;
+        indexes[number].push(index);
+    }
+
+    int find(int number) {
+        auto finder = indexes.find(number);
+        if (finder == end(indexes)) {
+            return -1;
+        } else {
+            auto& ref = finder->second;
+            while (!ref.empty() && values[ref.top()] != number) {
+                ref.pop();
+            }
+            return ref.empty() ? -1 : ref.top();
+        }
+    }
+};
+
+#endif
